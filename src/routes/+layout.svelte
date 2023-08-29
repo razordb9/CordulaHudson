d88dec716c082cac1fbacb4e54510355845a66b3
lauhard.andreas <lauhard.andreas@gmail.com>
2023-08-29 22:19:55 +0200

    added package npm i sass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import '../app.css';
+	import Naviation from '$lib/components/Naviation.svelte';
+    
+    
+</script>
+<div class="app">
+    <Naviation></Naviation>
+    <main>
+        <slot>
+            
+        </slot>
+    </main>
+</div>
+<style lang="scss">
+
+</style>
